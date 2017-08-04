fafb8fc3da258b852a2aa0999b35ce1f04b4c28f
isidro rm <isidro.rm.88@gmail.com>
2017-08-04 01:56:34 -0500

    Se agrega validaciones junto con mensajes para las mismas se agrega nueva vistas a donde enviar si las respuestas son correctas se agregan macros

--- Aplication/App.py
+++ Aplication/App.py
@@ -1,17 +1,42 @@
-from flask import Flask,render_template, request, make_response
+# -*- coding: utf-8 -*-
+from flask import *
 from Aplication.settings.db import conectionDb as db
+from Aplication.settings import forms
+import hashlib
+import datetime
+import sys,os
+from flask_wtf import CSRFProtect
 
-app = Flask(__name__)
 db = db()
+app = Flask(__name__)
+app.secret_key = 'angelesyoshua'
+csrf = CSRFProtect(app)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def ini():
-	r = db.rawQuery('SELECT * FROM usuarios')
-	print(r)
-	return render_template('login.html')
+	#r = db.rawQuery('SELECT * FROM usuarios')
+	#print(r)
+	oment_form = forms.iniForm(request.form)
+	return render_template('login.html', form = oment_form)
 
-#@app.route("/login", methods=['GET', 'POST'])
-#def login():
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+	_form = forms.iniForm(request.form)
+	if (request.method in ['POST'] and _form.validate()):
+		r = db.rawQuery("SELECT * FROM usuarios WHERE email = TRIM('{0}')".format(_form.email.data))
+		print(r)
+		if (r == []):
+			flash('El usuario no existe')
+			return render_template('login.html', form = _form)
+		for _data in r:
+			password = _data[3]
+			_compare = (_form.password.data).encode()
+			if (password != hashlib.sha224(_compare).hexdigest()):
+				flash('La contraseña es incorrecta')
+				return render_template('login.html', form = _form)
+		return render_template('index.html', form = _form)
+	#r_make = make_response()
+	return render_template('login.html', form = _form)
 
 """
 Modo de agregar password con cifrado
---
+++ Aplication/settings/forms.py
@@ -0,0 +1,27 @@
+from wtforms import Form
+from wtforms import StringField, TextField,TextAreaField,PasswordField
+from wtforms.fields.html5 import EmailField
+from wtforms import validators
+from wtforms import HiddenField
+
+#def 
+
+class iniForm(Form):
+    """docstring for iniForm"""
+    #def __init__(self, arg):
+    #	super(iniForm, self).__init__()
+    #	self.arg = arg
+    #username = StringField('Nombre', 
+    #                        [
+    #                        validators.length(min=4, max=25, message= '¡Ingrese un usuario valido!'),
+    #                        validators.Required(message = 'El usuario es requerido.')
+    #                        ])
+    email = EmailField('Correo electronico',
+                       [
+                        validators.Email(message='¡Ingrese un email valido!'),
+                        validators.Required(message = 'El email es requerido.')
+                       ])
+    password = PasswordField('Password',
+                            [
+                            validators.Required(message='¡Es necesario ingresar contraseña!')
+                            ])
